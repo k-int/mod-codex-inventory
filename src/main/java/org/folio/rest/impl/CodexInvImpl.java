f25b75920589d1ab17ad7041d1d4ee2317de60ca
Adam Dickmeiss <adam@indexdata.dk>
2017-12-19 14:28:44 +0100

    Almost complete conversion with all types fetched

--- src/main/java/org/folio/rest/impl/CodexInvImpl.java
+++ src/main/java/org/folio/rest/impl/CodexInvImpl.java
@@ -7,11 +7,13 @@ import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpClient;
 import io.vertx.core.http.HttpClientRequest;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import javax.ws.rs.core.Response;
 import org.folio.okapi.common.XOkapiHeaders;
@@ -42,7 +44,9 @@ public class CodexInvImpl implements CodexInstancesResource {
     });
     req.setChunked(true);
     for (Map.Entry<String, String> e : okapiHeaders.entrySet()) {
-      req.putHeader(e.getKey(), e.getValue());
+      if (!e.getKey().equalsIgnoreCase(XOkapiHeaders.URL)) {
+        req.putHeader(e.getKey(), e.getValue());
+      }
     }
     req.putHeader("Accept", "application/json");
     req.exceptionHandler(r -> {
@@ -50,6 +54,97 @@ public class CodexInvImpl implements CodexInstancesResource {
       fut.handle(Future.failedFuture(r.getMessage()));
     });
     req.end();
+  }
+
+  static Map<String, String> contributorTypeMap = new LinkedHashMap<>();
+  static Map<String, String> instanceTypeMap = new LinkedHashMap<>();
+  static Map<String, String> instanceFormatMap = new LinkedHashMap<>();
+  static Map<String, String> identifierTypeMap = new LinkedHashMap<>();
+
+  private void getMap(Context vertxContext, LHeaders headers, Map<String, String> map,
+    String path, String rootElement, Handler<AsyncResult<Void>> fut) {
+
+    HttpClient client = vertxContext.owner().createHttpClient();
+    int offset = map.size();
+    int chunk = 10;
+    final String url = headers.get(XOkapiHeaders.URL) + path
+      + "?limit=" + Integer.toString(chunk)
+      + "&offset=" + Integer.toString(offset);
+    logger.info("GetMap " + url);
+    getUrl(url, client, headers, res -> {
+      if (res.succeeded()) {
+        try {
+          JsonObject j = new JsonObject(res.result().toString());
+          JsonArray a = j.getJsonArray(rootElement);
+          if (a == null) {
+            fut.handle(Future.failedFuture("missing " + rootElement));
+            return;
+          }
+          logger.info(rootElement);
+          for (int i = 0; i < a.size(); i++) {
+            JsonObject ji = a.getJsonObject(i);
+            final String id = ji.getString("id");
+            final String name = ji.getString("name");
+            logger.info(" " + id + "=" + name);
+            map.put(id, name);
+          }
+          if (map.isEmpty()) {
+            fut.handle(Future.failedFuture(rootElement + " is empty"));
+          } else if (a.size() == chunk) {
+            getMap(vertxContext, headers, map, path, rootElement, fut);
+          } else {
+            fut.handle(Future.succeededFuture());
+          }
+        } catch (Exception e) {
+          fut.handle(Future.failedFuture(e));
+        }
+      } else {
+        fut.handle(Future.failedFuture(res.cause()));
+      }
+    });
+  }
+
+  private void getMaps(Context context, LHeaders headers, Handler<AsyncResult<Void>> fut) {
+    if (contributorTypeMap.isEmpty()) {
+      getMap(context, headers, contributorTypeMap, "/contributor-types", "contributorTypes",
+        res -> {
+          if (res.succeeded()) {
+            getMaps(context, headers, fut);
+          } else {
+            fut.handle(Future.failedFuture(res.cause()));
+          }
+        });
+    } else if (instanceTypeMap.isEmpty()) {
+      getMap(context, headers, instanceTypeMap, "/instance-types", "instanceTypes",
+        res -> {
+          if (res.succeeded()) {
+            getMaps(context, headers, fut);
+          } else {
+            fut.handle(Future.failedFuture(res.cause()));
+          }
+        });
+    } else if (instanceFormatMap.isEmpty()) {
+      getMap(context, headers, instanceFormatMap, "/instance-formats", "instanceFormats",
+        res -> {
+          if (res.succeeded()) {
+            getMaps(context, headers, fut);
+          } else {
+            fut.handle(Future.failedFuture(res.cause()));
+          }
+        });
+    } else if (identifierTypeMap.isEmpty()) {
+      getMap(context, headers, identifierTypeMap, "/identifier-types", "identifierTypes",
+        res -> {
+          if (res.succeeded()) {
+            getMaps(context, headers, fut);
+          } else {
+            fut.handle(Future.failedFuture(res.cause()));
+          }
+        });
+    } else {
+      logger.info("All maps fetched");
+      fut.handle(Future.succeededFuture());
+    }
   }
 
   private void getByQuery(Context vertxContext, String query,
@@ -74,13 +169,15 @@ public class CodexInvImpl implements CodexInstancesResource {
         fut.handle(Future.failedFuture(res.cause()));
       } else {
         Buffer b = res.result();
-        if (b.length() > 0) {
-          try {
-            InstanceConvert.invToCollection(new JsonObject(b.toString()), col);
-          } catch (Exception e) {
-            fut.handle(Future.failedFuture(e));
-            return;
-          }
+        logger.info("getByQuery succeeded. Analyzing results");
+        try {
+          InstanceConvert.invToCollection(new JsonObject(b.toString()), col,
+            contributorTypeMap, instanceTypeMap,
+            instanceFormatMap, identifierTypeMap);
+        } catch (Exception e) {
+          logger.warn(e);
+          fut.handle(Future.failedFuture(e.getMessage()));
+          return;
         }
         fut.handle(Future.succeededFuture());
       }
@@ -101,10 +198,13 @@ public class CodexInvImpl implements CodexInstancesResource {
         try {
           if (res.result().length() > 0) {
             JsonObject j = new JsonObject(res.result().toString());
-            InstanceConvert.invToCodex(j, instance);
+            InstanceConvert.invToCodex(j, instance,
+              contributorTypeMap, instanceTypeMap,
+              instanceFormatMap, identifierTypeMap);
           }
         } catch (Exception e) {
-          fut.handle(Future.failedFuture(e));
+          logger.warn(e);
+          fut.handle(Future.failedFuture(e.getMessage()));
           return;
         }
         fut.handle(Future.succeededFuture());
@@ -119,38 +219,53 @@ public class CodexInvImpl implements CodexInstancesResource {
 
     logger.info("GetCodexInstances");
     LHeaders lHeaders = new LHeaders(okapiHeaders);
-    InstanceCollection col = new InstanceCollection();
-    getByQuery(vertxContext, query, offset, limit, lHeaders, col, res -> {
-      if (res.failed()) {
+
+    getMaps(vertxContext, lHeaders, res1 -> {
+      if (res1.failed()) {
         handler.handle(Future.succeededFuture(
-                CodexInstancesResource.GetCodexInstancesResponse.withPlainInternalServerError(res.cause().getMessage())));
+          CodexInstancesResource.GetCodexInstancesResponse.withPlainInternalServerError(res1.cause().getMessage())));
       } else {
-        handler.handle(Future.succeededFuture(
-                CodexInstancesResource.GetCodexInstancesResponse.withJsonOK(col)));
+        InstanceCollection col = new InstanceCollection();
+        getByQuery(vertxContext, query, offset, limit, lHeaders, col, res2 -> {
+          if (res2.failed()) {
+            handler.handle(Future.succeededFuture(
+              CodexInstancesResource.GetCodexInstancesResponse.withPlainInternalServerError(res2.cause().getMessage())));
+          } else {
+            handler.handle(Future.succeededFuture(
+              CodexInstancesResource.GetCodexInstancesResponse.withJsonOK(col)));
+          }
+        });
       }
     });
   }
 
   @Override
   public void getCodexInstancesById(String id, String lang,
-          Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> handler,
-          Context vertxContext) throws Exception {
+    Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> handler,
+    Context vertxContext) throws Exception {
 
     logger.info("GetCodexInstancesById");
     LHeaders lHeaders = new LHeaders(okapiHeaders);
-    Instance instance = new Instance();
-    getById(id, vertxContext, lHeaders, instance, res -> {
-      if (res.failed()) {
+    getMaps(vertxContext, lHeaders, res1 -> {
+      if (res1.failed()) {
         handler.handle(Future.succeededFuture(
-                CodexInstancesResource.GetCodexInstancesByIdResponse.withPlainInternalServerError(res.cause().getMessage())));
+          CodexInstancesResource.GetCodexInstancesResponse.withPlainInternalServerError(res1.cause().getMessage())));
       } else {
-        if (instance.getId() == null) {
-          handler.handle(Future.succeededFuture(
-                  CodexInstancesResource.GetCodexInstancesByIdResponse.withPlainNotFound(id)));
-        } else {
-          handler.handle(Future.succeededFuture(
-                  CodexInstancesResource.GetCodexInstancesByIdResponse.withJsonOK(instance)));
-        }
+        Instance instance = new Instance();
+        getById(id, vertxContext, lHeaders, instance, res2 -> {
+          if (res2.failed()) {
+            handler.handle(Future.succeededFuture(
+              CodexInstancesResource.GetCodexInstancesByIdResponse.withPlainInternalServerError(res2.cause().getMessage())));
+          } else {
+            if (instance.getId() == null) {
+              handler.handle(Future.succeededFuture(
+                CodexInstancesResource.GetCodexInstancesByIdResponse.withPlainNotFound(id)));
+            } else {
+              handler.handle(Future.succeededFuture(
+                CodexInstancesResource.GetCodexInstancesByIdResponse.withJsonOK(instance)));
+            }
+          }
+        });
       }
     });
   }
--- src/main/java/org/folio/rest/impl/InstanceConvert.java
+++ src/main/java/org/folio/rest/impl/InstanceConvert.java
@@ -2,8 +2,15 @@ package org.folio.rest.impl;
 
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.folio.rest.jaxrs.model.Contributor;
+import org.folio.rest.jaxrs.model.Identifier;
 import org.folio.rest.jaxrs.model.Instance;
 import org.folio.rest.jaxrs.model.Instance.Type;
 import org.folio.rest.jaxrs.model.InstanceCollection;
@@ -12,8 +19,15 @@ public class InstanceConvert {
   private InstanceConvert() {
     throw new IllegalStateException("Instance");
   }
+  private static final Logger logger = LoggerFactory.getLogger("codex.inventory");
 
-  public static void invToCollection(JsonObject j, InstanceCollection col) {
+  public static void invToCollection(JsonObject j, InstanceCollection col,
+    Map<String, String> contributorTypeMap,
+    Map<String, String> instanceTypeMap,
+    Map<String, String> instanceFormatMap,
+    Map<String, String> identifierTypeMap) {
+
+    logger.info("invToCollection");
     JsonArray a = j.getJsonArray("instances");
     if (a == null) {
       throw (new IllegalArgumentException("instances"));
@@ -21,7 +35,8 @@ public class InstanceConvert {
     List<Instance> l = new LinkedList<>();
     for (int i = 0; i < a.size(); i++) {
       Instance instance = new Instance();
-      invToCodex(a.getJsonObject(i), instance);
+      invToCodex(a.getJsonObject(i), instance, contributorTypeMap,
+        instanceTypeMap, instanceFormatMap, identifierTypeMap);
       l.add(instance);
     }
     col.setInstances(l);
@@ -32,15 +47,22 @@ public class InstanceConvert {
     col.setTotalRecords(cnt);
   }
 
-  public static void invToCodex(JsonObject j, Instance instance) {
-    {
+  public static void invToCodex(JsonObject j, Instance instance,
+    Map<String, String> contributorTypeMap,
+    Map<String, String> instanceTypeMap,
+    Map<String, String> instanceFormatMap,
+    Map<String, String> identifierTypeMap) {
+
+    logger.info("invToCodex");
+    { // required in codex
       final String id = j.getString("id");
       if (id == null) {
         throw (new IllegalArgumentException("id missing"));
       }
       instance.setId(id);
     }
-    {
+
+    { // required in codex
       final String title = j.getString("title");
       if (title == null) {
         throw (new IllegalArgumentException("title missing"));
@@ -59,14 +81,120 @@ public class InstanceConvert {
         instance.setSeries(ar.getString(0));
       }
     }
-    instance.setType(Type.BOOKS); // TODO: instanceTypeId
+    // creators
+
     {
+      JsonArray ar = j.getJsonArray("contributors");
+      if (ar != null) {
+        Set<Contributor> cl = new HashSet<>();
+        for (int i = 0; i < ar.size(); i++) {
+          JsonObject ji = ar.getJsonObject(i);
+          final String type = contributorTypeMap.get(ji.getString("contributorTypeId"));
+          if (type != null) {
+            Contributor c = new Contributor();
+            c.setName(ji.getString("name"));
+            c.setType(type);
+            cl.add(c);
+          }
+        }
+        instance.setContributor(cl);
+      }
+    }
+
+    {
+      JsonArray ar = j.getJsonArray("publication");
+      if (ar != null && ar.size() > 0) {
+        JsonObject ji = ar.getJsonObject(0);
+        final String publisher = ji.getString("publisher");
+        if (publisher != null) {
+          instance.setPublisher(publisher);
+        }
+        final String date = ji.getString("dateOfPublication");
+        if (date != null) {
+          instance.setDate(date);
+        }
+      }
+    }
+
+    {
+      // required in codex
+      final String id = j.getString("instanceTypeId");
+      if (id == null) {
+        throw (new IllegalArgumentException("instanceTypeId missing"));
+      }
+      final String name = instanceTypeMap.get(id);
+      if (name == null) {
+        throw (new IllegalArgumentException("instanceTypeId " + id + " does not exist"));
+      }
+      final String nName = name.toUpperCase().replaceAll("\\s", "");
+      try {
+        instance.setType(Type.valueOf(nName));
+      } catch (IllegalArgumentException ex) {
+        instance.setType(Type.UNSPECIFIED);
+      }
+    }
+
+    {
+      final String id = j.getString("instanceFormatId");
+      if (id != null) {
+        final String format = instanceFormatMap.get(id);
+        if (format == null) {
+          throw (new IllegalArgumentException("instanceFormatId " + id + " does not exist"));
+        }
+        instance.setFormat(format);
+      }
+    }
+
+    {
+      JsonArray ar = j.getJsonArray("identifiers");
+      if (ar != null) {
+        Set<Identifier> il = new HashSet<>();
+        for (int i = 0; i < ar.size(); i++) {
+          JsonObject ji = ar.getJsonObject(i);
+          final String type = identifierTypeMap.get(ji.getString("identifierTypeId"));
+          if (type != null) {
+            Identifier identifier = new Identifier();
+            identifier.setType(type);
+            identifier.setValue(ji.getString("value"));
+            il.add(identifier);
+          }
+        }
+        instance.setIdentifier(il);
+      }
+    }
+
+    { // required in codex
       final String source = j.getString("source");
       if (j == null) {
         throw (new IllegalArgumentException("source missing"));
       }
       instance.setSource(source);
     }
-    // TODO: More conversions
+
+    {
+      JsonArray ar = j.getJsonArray("languages");
+      if (ar != null) {
+        List<String> il = new LinkedList<>();
+        for (int i = 0; i < ar.size(); i++) {
+          final String lang = ar.getString(i);
+          il.add(lang);
+        }
+        instance.setLanguage(il);
+      }
+    }
+    // Element rights does not exist in inventory
+    {
+      final String edition = j.getString("edition");
+      if (edition != null) {
+        instance.setVersion(edition);
+      }
+    }
+
+    { // TODO: modifiedDate not found in inventory
+      final String l = j.getString("modifiedDate");
+      if (l != null) {
+        instance.setLastModified(l);
+      }
+    }
   }
 }
